f12a6a758d5812f8879802fb0d4506aab8d45f00
danhamill <hamill.daniel@gmail.com>
2015-05-12 15:28:52 -0600

    converted from grainsize script

--- convert_XYZ_To_Shp.py
+++ convert_XYZ_To_Shp.py
@@ -5,7 +5,7 @@ import osgeo.osr as osr
 import csv
 
 # use a dictionary reader so we can access by field name
-reader = csv.DictReader(open("NO_Reach4a_May2009_eyeball_grainsize.csv","rb"),
+reader = csv.DictReader(open("Segment_029_2009_x_y_sedclass_dt_topo_25cm.xyz","rb"),
     delimiter=',',
     quoting=csv.QUOTE_NONE)
 
@@ -13,32 +13,30 @@ reader = csv.DictReader(open("NO_Reach4a_May2009_eyeball_grainsize.csv","rb"),
 driver = ogr.GetDriverByName("ESRI Shapefile")
 
 # create the data source
-data_source = driver.CreateDataSource("AZFIPS.shp")
+data_source = driver.CreateDataSource("MB_Sed_Class.shp")
 
 # create the spatial reference, AZ Central SP
 srs = osr.SpatialReference()
-srs.ImportFromProj4('+proj=tmerc +lat_0=31 +lon_0=-111.9166666666667 +k=0.9999 +x_0=213360 +y_0=0 +ellps=GRS80 +units=m +no_defs ')
+srs.ImportFromESPG(26949)
 
 # create the layer
-layer = data_source.CreateLayer("AZFIPS", srs, ogr.wkbPoint)
+layer = data_source.CreateLayer("MB_Sed_Class", srs, ogr.wkbPoint)
 
 # Add the fields we're interested in
-field_name = ogr.FieldDefn("Date", ogr.OFTString)
+field_name = ogr.FieldDefn("Easting", ogr.OFTReal)
 field_name.SetWidth(24)
 layer.CreateField(field_name)
-layer.CreateField(ogr.FieldDefn("Easting", ogr.OFTReal))
 layer.CreateField(ogr.FieldDefn("Northing", ogr.OFTReal))
-layer.CreateField(ogr.FieldDefn("p=50", ogr.OFTReal))
+layer.CreateField(ogr.FieldDefn("Sed_Class", ogr.OFTReal))
 
 # Process the text file and add the attributes and features to the shapefile
 for row in reader:
   # create the feature
   feature = ogr.Feature(layer.GetLayerDefn())
   # Set the attributes using the values from the delimited text file
-  feature.SetField("Date", row['Date'])
   feature.SetField("Easting", row['Easting'])
   feature.SetField("Northing", row['Northing'])
-  feature.SetField("p=50", row['p=50'])
+  feature.SetField("Sed_Class", row['Sed_Class'])
 
   # create the WKT for the feature using Python string formatting
   wkt = "POINT(%f %f)" %  (float(row['Northing']) , float(row['Easting']))
