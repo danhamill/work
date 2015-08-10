66cc2994f927dfa8bf0be34c4566c6408779a7bd
danhamill <hamill.daniel@gmail.com>
2015-08-10 17:07:31 -0600

    Fixed to import directly from asc files

--- _gfname.sh
+++ _gfname.sh
@@ -3,41 +3,41 @@
 
 
 reach='R4a'
+ss='r4a'
 pkey='_pkey'
 gist='_the_geom_gist'
 files=$(find D:/R4a/2015_04/ -name "x_y_class*")
 rpkey=$reach$pkey
 rgist=$reach$gist
 
-#create temp import table
-psql -h localhost -d $reach -U postgres -p 5432 -c "CREATE TABLE tmp(
-    easting double precision,
-    northing double precision,
-    texture double precision);"
 
 #Create blank table for importing csv's too
-# psql -h localhost -d $reach -U postgres -p 5432 -c "CREATE TABLE "$reach"(
-    # easting double precision,
-    # northing double precision,
-    # texture double precision,
-    # gid serial NOT NULL,
-    # the_geom geometry,
-    # CONSTRAINT "$rpkey" PRIMARY KEY (gid),
-    # CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2),
-    # CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL),
-    # CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 26949));"
-
-# psql -h localhost -d $reach -U postgres -p 5432 -c "CREATE INDEX "$rgist" ON "$reach" USING gist (the_geom );"
+psql -h localhost -d $reach -U postgres -p 5432 -c "CREATE TABLE "$ss"(
+  easting double precision,
+  northing double precision,
+  texture double precision);"
 
 #loop through all x_y_class*.asc files for the supplied reach    
 for file in $files:
 do
-    echo $file
-    if [ -f $file ]; then
-        echo "That directory exists"
-        psql -h localhost -d $reach -U postgres -p 5432 -c "\COPY tmp FROM "$file" DELIMITER ' ' CSV;"
-    else
-        echo "That directory doesn't exists"
-    fi
-    #
+  echo $file
+  if [ -f $file ]; then
+    echo "That directory exists"
+    psql -h localhost -d $reach -U postgres -p 5432 -c "\COPY "$ss" FROM "$file" DELIMITER ' ' CSV;"
+  else
+    echo "That directory doesn't exists"
+  fi
+  
 done
+
+#Add geom field to table
+psql -h localhost -d $reach -U postgres -p 5432 -c "ALTER TABLE "$ss" ADD COLUMN the_geom GEOMETRY
+  CONSTRAINT enforce_dims_the_geom CHECK (st_ndims(the_geom) = 2)
+  CONSTRAINT enforce_geotype_geom CHECK (geometrytype(the_geom) = 'POINT'::text OR the_geom IS NULL)
+  CONSTRAINT enforce_srid_the_geom CHECK (st_srid(the_geom) = 26949);"
+
+#Add Index to table
+psql -h localhost -d $reach -U postgres -p 5432 -c "CREATE INDEX "$rgist" ON "$ss" USING gist (the_geom);"
+
+#Populate Geometry field 
+psql -h localhost -d $reach -U postgres -p 5432 -c "update "$ss" SET the_geom = ST_GeomFromText('POINT(' || easting || ' ' || northing || ')', 26949);"
