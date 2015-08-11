6bdb3bdbc33c0cda8f2decf0beb01fc6233d9a93
danhamill <hamill.daniel@gmail.com>
2015-08-11 17:32:02 -0600

    general editing and encoded with utf 8

--- _convert_XYZ_To_Shp.py
+++ _convert_XYZ_To_Shp.py
@@ -15,55 +15,54 @@ out = "C:\\workspace\\Reach_4a\\test_out\\"
 list = glob(root + '/*.xyz')
 # use a dictionary reader so we can access by field name
 for afile in list:
-	print (afile + "\n")
-	reader = csv.DictReader(open(afile,"rb"),
-		fieldnames=("Easting","Northing","Sed_Class"),
-		delimiter=',',
-		quoting=csv.QUOTE_NONE)
-	
-	# set up the shapefile driver
-	driver = ogr.GetDriverByName("ESRI Shapefile")
-	
-	filename = os.path.basename(os.path.splitext(afile)[0])
-	print filename
+    print (afile + "\n")
+    reader = csv.DictReader(open(afile,"rb"),
+        fieldnames=("Easting","Northing","Sed_Class"),
+        delimiter=',',
+        quoting=csv.QUOTE_NONE)
+    
+    # set up the shapefile driver
+    driver = ogr.GetDriverByName("ESRI Shapefile")
+    
+    filename = os.path.basename(os.path.splitext(afile)[0])
     #create the data source
-	data_source = driver.CreateDataSource(out + filename + ".shp")
-	
-	# create the spatial reference, AZ Central SP
-	srs = osr.SpatialReference()
-	srs.ImportFromEPSG(26949)
-	
-	# create the layer
-	layer = data_source.CreateLayer(filename, srs, ogr.wkbPoint)
-	
-	# Add the fields we're interested in
-	field_name = ogr.FieldDefn("Easting", ogr.OFTReal)
-	#field_name.SetWidth(100)
-	layer.CreateField(field_name)
-	layer.CreateField(ogr.FieldDefn("Northing", ogr.OFTReal))
-	layer.CreateField(ogr.FieldDefn("Sed_Class", ogr.OFTReal))
-	
-	# Process the text file and add the attributes and features to the shapefile
-	for row in reader:
-		# create the feature
-		feature = ogr.Feature(layer.GetLayerDefn())
-		# Set the attributes using the values from the delimited text file
-		feature.SetField("Easting", row['Easting'])
-		feature.SetField("Northing", row['Northing'])
-		feature.SetField("Sed_Class", row['Sed_Class'])
-	
-		# create the WKT for the feature using Python string formatting
-		wkt = "POINT(%f %f)" %  (float(row['Easting']) , float(row['Northing']))
-	
-		# Create the point from the Well Known Txt
-		point = ogr.CreateGeometryFromWkt(wkt)
-	
-		# Set the feature geometry using the point
-		feature.SetGeometry(point)
-		# Create the feature in the layer (shapefile)
-		layer.CreateFeature(feature)
-		# Destroy the feature to free resources
-		feature.Destroy()
-	
-	# Destroy the data source to free resources
-	data_source.Destroy()
+    data_source = driver.CreateDataSource(out + filename + ".shp")
+    
+    # create the spatial reference, AZ Central SP
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(26949)
+    
+    # create the layer
+    layer = data_source.CreateLayer(filename, srs, ogr.wkbPoint)
+    
+    # Add the fields we're interested in
+    field_name = ogr.FieldDefn("Easting", ogr.OFTReal)
+    #field_name.SetWidth(100)
+    layer.CreateField(field_name)
+    layer.CreateField(ogr.FieldDefn("Northing", ogr.OFTReal))
+    layer.CreateField(ogr.FieldDefn("Sed_Class", ogr.OFTReal))
+    
+    # Process the text file and add the attributes and features to the shapefile
+    for row in reader:
+        # create the feature
+        feature = ogr.Feature(layer.GetLayerDefn())
+        # Set the attributes using the values from the delimited text file
+        feature.SetField("Easting", row['Easting'])
+        feature.SetField("Northing", row['Northing'])
+        feature.SetField("Sed_Class", row['Sed_Class'])
+    
+        # create the WKT for the feature using Python string formatting
+        wkt = "POINT(%f %f)" %  (float(row['Easting']) , float(row['Northing']))
+    
+        # Create the point from the Well Known Txt
+        point = ogr.CreateGeometryFromWkt(wkt)
+    
+        # Set the feature geometry using the point
+        feature.SetGeometry(point)
+        # Create the feature in the layer (shapefile)
+        layer.CreateFeature(feature)
+        # Destroy the feature to free resources
+        feature.Destroy()
+    
+    # Destroy the data source to free resources
+    data_source.Destroy()
