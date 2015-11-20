4ee2aad926f5d8355a899e84c6ad0e9fd8cf4c88
danhamill <hamill.daniel@gmail.com>
2015-11-20 16:54:22 -0700

    Added functionality to export gridded results

--- resample_asc_files.py
+++ resample_asc_files.py
@@ -4,37 +4,41 @@ import numpy as np
 import pyproj
 
 
-area_id = 'r4a'
-name = 'Reach 4a grid'
-proj_id = 'EPSG:26949'
-proj4_args = 'proj=tmerc, +lat_0=31, +lon_0=-111.9166666666667, +k=0.9999, +x_0=213360, +y_0=0, +ellps=GRS80, +datum=NAD83, +units=m, +no_defs'
-x_size = 400
-y_size = 800
-area_extent = (223000,578100,223100,578300)
-proj_dict = {'proj': 'tmerc', 'lat_0': '31', 'lon_0': '-111.9166666666667', 'k': '0.9999', 'x_0': '213360', 'ellps':'GRS80', 'datum':'NAD83', 'units':'m', 'no_defs':''}
-area_def = geometry.AreaDefinition(area_id, name, proj_id, proj_dict, x_size, y_size, area_extent)
-print area_def
-
 trans =  pyproj.Proj(init="epsg:26949")
 
-fname = "E:/R4a/2012_05/R00034/x_y_class0.asc"
-names = "Easting,Northing,Texture"
-d = np.genfromtxt(fname, dtype=float, delimiter =' ', names=names)
-data = np.vstack(d.flatten())
-humlon, humlat = trans(d['Easting'],d['Northing'],inverse=True)
-orig_def = geometry.SwathDefinition(lons=humlon.flatten(), lats=humlat.flatten())
-
-res = 0.25
-grid_x, grid_y = np.meshgrid( np.arange(np.floor(np.min(d['Easting'])), np.ceil(np.max(d['Easting'])), res), np.arange(np.floor(np.min(d['Northing'])), np.ceil(np.max(d['Northing'])), res) )
-longrid, latgrid = trans(grid_x, grid_y, inverse=True)
-target_def = geometry.SwathDefinition(lons=longrid.flatten(), lats=latgrid.flatten())
-
-result = kd_tree.resample_nearest(orig_def, d['Texture'].flatten(), target_def, radius_of_influence=1, fill_value=None, nprocs = cpu_count())
-
-gridded_result = np.reshape(result,np.shape(longrid))
+fIn = "C:\\users\\dan\\Reach_4a\\humminbird\\April2015\\R02027\\x_y_class0.asc"
+fOut = 'C:\\users\\dan\\Reach_4a\\humminbird\\April2015\\R02027\\x_y_class0_gridded.asc'
 
 
-result = kd_tree.resample_nearest(orig_def, d['Texture'].flatten(), area_def, radius_of_influence=1, fill_value=None, nprocs = cpu_count())
+def ascol( arr ):
+   '''
+   reshapes row matrix to be a column matrix (N,1).
+   '''
+   if len( arr.shape ) == 1: arr = arr.reshape( ( arr.shape[0], 1 ) )
+   return arr 
 
+def trythis(fOut,fIn):
+    names = "Easting,Northing,Texture"
+    d = np.genfromtxt(fIn, dtype=float, delimiter =' ', names=names)
+    data = np.vstack(d.flatten())
+    humlon, humlat = trans(d['Easting'],d['Northing'],inverse=True)
+    orig_def = geometry.SwathDefinition(lons=humlon.flatten(), lats=humlat.flatten())
+    
+    res = 0.25
+    grid_x, grid_y = np.meshgrid( np.arange(np.floor(np.min(d['Easting'])), np.ceil(np.max(d['Easting'])), res), np.arange(np.floor(np.min(d['Northing'])), np.ceil(np.max(d['Northing'])), res) )
+    longrid, latgrid = trans(grid_x, grid_y, inverse=True)
+    target_def = geometry.SwathDefinition(lons=longrid.flatten(), lats=latgrid.flatten())
+    
+    result = kd_tree.resample_nearest(orig_def, d['Texture'].flatten(), target_def, radius_of_influence=1, fill_value=None, nprocs = cpu_count())
+    
+    del d
+    
+    gridded_result = np.reshape(result,np.shape(longrid))
+    mask = gridded_result.mask==True
+    with open(fOut, 'wb')as f:
+        np.savetxt(f, np.hstack((ascol(grid_x[mask==False].flatten()),ascol(grid_y[mask==False].flatten()),ascol(gridded_result[mask==False].flatten()))),delimiter=' ', fmt="%8.6f %8.6f %1.6f")
+    f.close()
+    
 
-
+if  __name__ == '__main__':
+    trythis(fOut,fIn)
